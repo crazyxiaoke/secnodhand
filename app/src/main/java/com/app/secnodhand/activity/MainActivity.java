6c23e543b839669b33d1bc38f4988b50d7d09fc8
xiaoke9120 <651617482@qq.com>
2015-05-28 17:31:45 +0800

    添加百度地图

--- app/src/main/java/com/app/secnodhand/activity/MainActivity.java
+++ app/src/main/java/com/app/secnodhand/activity/MainActivity.java
@@ -17,7 +17,11 @@ import com.app.secnodhand.base.ViewInject;
 import com.app.secnodhand.entity.LocationInfo;
 import com.baidu.location.BDLocation;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 /**
  * Created by zxk on 15-5-26.
@@ -27,6 +31,7 @@ public class MainActivity extends BaseActivity{
     @ViewInject(R.id.main_mapview)
     private MapView mMapView;
     private BaiduMap mBaiduMap;
+    private MyLocationData locData;
 
 
     @Override
@@ -43,6 +48,7 @@ public class MainActivity extends BaseActivity{
     private void initView(){
         mBaiduMap=mMapView.getMap();
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.zoomTo(15.5f));
     }
 
     @Override
@@ -75,6 +81,18 @@ public class MainActivity extends BaseActivity{
                         LocationInfo locInfo = LocationInfo
                                 .fromBDLocationToLocationInfo(location);
                         MyApplication.sLocInfo = locInfo;
+                        locData = new MyLocationData.Builder().accuracy(locInfo.getAccuracy())
+                                .direction(100).latitude(locInfo.getLatitude())
+                                .longitude(locInfo.getLongitude()).build();
+                        Log.d("TAG","lat="+locInfo.getLatitude());
+                        Log.d("TAG","lng="+locInfo.getLongitude());
+                        if(mBaiduMap!=null){
+                            mBaiduMap.setMyLocationEnabled(true);
+                            mBaiduMap.setMyLocationData(locData);
+                            LatLng ll = new LatLng(locInfo.getLatitude(), locInfo.getLongitude());
+                            MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+                            mBaiduMap.animateMapStatus(u);
+                        }
                     }
                 });
     }
