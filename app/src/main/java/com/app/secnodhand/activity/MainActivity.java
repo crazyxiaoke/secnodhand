5ac67cd0cb3728f892c56e97f70f0e0cfdccea65
xiaoke9120 <651617482@qq.com>
2015-06-02 14:54:05 +0800

    bug fix

--- app/src/main/java/com/app/secnodhand/activity/MainActivity.java
+++ app/src/main/java/com/app/secnodhand/activity/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.app.secnodhand.BaiduMapManager;
@@ -30,8 +32,11 @@ public class MainActivity extends BaseActivity{
 
     @ViewInject(R.id.main_mapview)
     private MapView mMapView;
+    @ViewInject(R.id.main_menu)
+    private TextView menu;
     private BaiduMap mBaiduMap;
     private MyLocationData mLocData;
+    private TextView text;
 
 
     @Override
@@ -51,6 +56,16 @@ public class MainActivity extends BaseActivity{
         mMapView.showScaleControl(false);
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
         mBaiduMap.setMapStatus(MapStatusUpdateFactory.zoomTo(15.5f));
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text=new TextView(mContext);
+//                text.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.MATCH_PARENT));
+                text.setBackgroundColor(0xffff0000);
+                text.setAnimation(AnimationUtils.loadAnimation(mContext,R.anim.map_bz_anim));
+                addContentView(text,new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.MATCH_PARENT));
+            }
+        });
     }
 
     @Override
@@ -86,8 +101,6 @@ public class MainActivity extends BaseActivity{
                         mLocData = new MyLocationData.Builder().accuracy(locInfo.getAccuracy())
                                 .direction(100).latitude(locInfo.getLatitude())
                                 .longitude(locInfo.getLongitude()).build();
-                        Log.d("TAG","lat="+locInfo.getLatitude());
-                        Log.d("TAG","lng="+locInfo.getLongitude());
                         if(mBaiduMap!=null){
                             mBaiduMap.setMyLocationEnabled(true);
                             mBaiduMap.setMyLocationData(mLocData);
